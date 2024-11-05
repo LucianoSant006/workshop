4a95e82da682693b40486b2b30510cef418cfbc5
LucianoSant006 <santiagoluciano383@gmail.com>
2024-11-05 14:04:34 -0300

    Category

---
+++ src/main/java/com/example/web_workshop/controller/CategoryController.java
@@ -0,0 +1,4 @@
+package com.example.web_workshop.controller;
+
+public class CategoryController {
+}
---
+++ src/main/java/com/example/web_workshop/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.example.web_workshop.services;
+
+import com.example.web_workshop.entities.Category;
+import com.example.web_workshop.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll(){
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> obj = categoryRepository.findById(id);
+        return  obj.get();
+    }
+
+}
