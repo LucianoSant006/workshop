4df8e7ca18390328b8933ce540cdbfbb2fa1748d
LucianoSant006 <santiagoluciano383@gmail.com>
2024-11-05 20:27:43 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/example/web_workshop/controller/ProductController.java
+++ src/main/java/com/example/web_workshop/controller/ProductController.java
@@ -1,35 +1,33 @@
 package com.example.web_workshop.controller;
 
+import com.example.web_workshop.entities.Product;
 import com.example.web_workshop.entities.User;
-import com.example.web_workshop.repositories.UserRepository;
+import com.example.web_workshop.services.ProductService;
 import com.example.web_workshop.services.UserService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserController {
+@RequestMapping(value = "/products")
+public class ProductController {
 
     @Autowired
-    private UserService  userService;
+    private ProductService productService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-      List<User> list = userService.findAll();
+    public ResponseEntity<List<Product>> findAll(){
+      List<Product> list = productService.findAll();
       return ResponseEntity.ok().body(list);
     }
     @GetMapping(value = "{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-        User obj = userService.findById(id);
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+        Product obj = productService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
--- src/main/java/com/example/web_workshop/repositories/CategoryRepository.java
+++ src/main/java/com/example/web_workshop/repositories/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.example.web_workshop.repositories;
 
-import com.example.web_workshop.entities.Product;
+import com.example.web_workshop.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
